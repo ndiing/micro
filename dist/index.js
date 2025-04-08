bf648cf0c1269d720323d741804b48123a28430c
Ridho Prasetya <ndiing.inc@gmail.com>
2025-04-08 19:12:35 +0700

    expose dist example

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+function init(){
+    const client = new WebSocket('ws://localhost')
+    client.addEventListener('close',(event) => {
+        console.log(event)
+        setTimeout(init,1000)
+    })
+    client.addEventListener('error',console.log)
+    client.addEventListener('message',(event) => {
+        console.log(event)
+        // client.close()
+    })
+    client.addEventListener('open',(event) => {
+        console.log(event)
+        client.send(JSON.stringify({message:'from client'}))
+    })
+}
+init()
